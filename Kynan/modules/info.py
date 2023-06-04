dcfd634e7d1e09db8b10bf74af55ad065adcbec9
RITO <89874919+DjeFaris@users.noreply.github.com>
2023-06-04 13:37:20 +0700

    Update info.py

--- Kynan/modules/info.py
+++ Kynan/modules/info.py
@@ -61,6 +61,15 @@ async def get_chat_info(chat, already=False):
     caption = section("Chat info", body)
     return [caption, photo_id]
 
+@app.on_message(filters.command("clean") & DEV_USERS)
+async def clean(_, message):
+    dir = "downloads"
+    dir1 = "cache"
+    shutil.rmtree(dir)
+    shutil.rmtree(dir1)
+    os.mkdir(dir)
+    os.mkdir(dir1)
+    await message.reply_text("Successfully cleaned all **temp** dir(s)!")
 
 @app.on_message(filters.command("uinfo"))
 async def info_func(_, message: Message):
