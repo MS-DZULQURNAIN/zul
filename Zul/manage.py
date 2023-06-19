ad600d3c5f09ba101149a03864f0791448a532a4
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-06-19 16:27:06 +0700

    Update manage.py

--- Zul/manage.py
+++ Zul/manage.py
@@ -19,4 +19,60 @@ Perintah Dasar
 
 👮🏻 /info memberikan informasi tentang pengguna
 
-◽️ /staff memberikan Daftar lengkap Staf grup"""
+◽️ /staff memberikan Daftar lengkap Staf grup
+"""
+
+LANJUT = """
+**Perintah Lanjutan**
+
+🕵🏻 Tersedia untuk Admin
+👮🏻 Tersedia untuk Admin & Moderator
+🛃 Tersedia untuk Admin & Pembersih Obrolan
+
+**MANAJEMEN PERINGATAN**
+👮🏻 `/warn` memberikan peringatan ke pengguna
+👮🏻 `/resetwarn` menghapus 
+👮🏻 `/warnlist` memungkinkan anda melihat dan mengelola peringatan pengguna
+🕵🏻 `/dwarn` menghapus pesan dan memberikan peringatan ke pengguna
+
+🛃 `/del` menghapus pesan yang dipilih
+🛃 `/purge` sama seperti `/del` lebih efisien
+
+◽️ `/report` atau @admin melaporkan pengguna ke admin
+"""
+
+AHLI = """
+**Perintah Ahli**
+
+👥 Tersedia untuk semua pengguna
+👮🏻 Tersedia untuk Admin & Moderator
+🕵🏻 Tersedia untuk Admin
+
+👥 `/admins` melihat daftar admin
+👮🏻 `/admincache` memulai ulang cache admin
+
+**Pesan Tersemat**
+🕵🏻 `/pin` menyematkan pesan dengan membalas ke pesan.
+🕵🏻 `/unpin` menghapus pesan sematan.
+👥 `/pinned` melihat daftar pin yang ada di group
+"""
+
+PRO = """
+**Perintah pro**
+
+👥 Tersedia untuk semua pengguna
+👮🏻 Tersedia untuk admin & moderator
+🕵🏻 Tersedia untuk admin
+
+**Rules Group**
+👥 `/rules` melihat rules group
+👮🏻 `/setrules` pasang rules digroup
+👮🏻 `/clearrules` menghapus rules
+
+**Note group**
+👮🏻 `/notes` melihat notes digroup
+👮🏻 `/save [note name]` menyimpan note
+👮🏻 `/get [note name]` menampilkan note
+👮🏻 `/clear [note name]` hapus note name
+👮🏻 `/removeallnotes` hapus semua notes
+"""
