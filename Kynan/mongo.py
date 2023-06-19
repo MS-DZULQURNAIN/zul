20051c83b2e26693482662f413c673839bdf5fa8
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-06-19 16:23:59 +0700

    Update mongo.py

--- Kynan/mongo.py
+++ Kynan/mongo.py
@@ -4,19 +4,17 @@ from motor import motor_asyncio
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
 from Kynan import MONGO_DB_URI
-from Kynan.confing import get_int_key, get_str_key
 
 
-MONGO_PORT = get_int_key("27017")
-MONGO_DB_URI = get_str_key("MONGO_DB_URI")
-MONGO_DB = "Kynan"
+MONGO_PORT = "27017"
+MONGO_DB = "zul"
 
 
 client = MongoClient()
-client = MongoClient(MONGO_DB_URI, MONGO_PORT)[MONGO_DB]
-motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, MONGO_PORT)
+client = MongoClient(MONGO_DB_URI, 27017)[MONGO_DB]
+motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, 27017)
 db = motor[MONGO_DB]
-db = client["Kynan"]
+db = client["zul"]
 try:
     asyncio.get_event_loop().run_until_complete(motor.server_info())
 except ServerSelectionTimeoutError:
